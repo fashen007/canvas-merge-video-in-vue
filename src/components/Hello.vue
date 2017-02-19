6200eba58726def916d9cf730575c30c595ca78b
huangyong <646028751@qq.com>
2017-02-19 17:08:07 +0800

    顺播正常...

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <template v-for='(list, i) in playList'>
-      <video class="video" controls width="270" v-show='i === currentIndex'>
-        <source :src="list.src" type='video/mp4' key='i'>
+      <video class="video" controls width="270" v-show='i == currentIndex' style='position:absolute; left: 9999px;'>
+        <source :src="list.src" type='video/mp4' key='i' >
       </video>
     </template>
-    <div class='video-cont'>
-      <canvas controls id="myCanvas" width="270" height="135" style="border:1px solid #d3d3d3;" v-loading.body="loading">Your browser does not support the HTML5 canvas tag.</canvas>
+    <div class='video-cont' v-loading="loading">
+      <canvas controls id="myCanvas" width='400'height='200'style="border:1px solid #d3d3d3;">Your browser does not support the HTML5 canvas tag.</canvas>
       <div class='controls'>
           <el-row :gutter="5" class='row'>
             <el-col :span="3"><div class="grid-content bg-purple"></div><el-button size="mini" @click.native='triggerPlay'>{{pauseing ? '开始': '暂停'}}</el-button></el-col>
             <el-col :span="7" class='time-duration'><div class="grid-content bg-purple">{{currentTime}}/{{lastTime}}</div></el-col>
-            <el-col :span="10"><div class="grid-content bg-purple"><el-slider @change='progressChange' v-model="progress" :max='allLength'></el-slider></div></el-col>
+            <el-col :span="10"><div class="grid-content bg-purple"><el-slider @change='progressDrag' v-model="progress" :max='allLength'></el-slider></div></el-col>
           </el-row>
       </div>
     </div>
@@ -68,62 +67,85 @@ export default {
   },
   watch: {
     progress: function(newVal, oldVal) {
-      let diff = Number(newVal - oldVal).toFixed(2)
+      let diff = Number(Math.abs(newVal - oldVal)).toFixed(2)
       if(diff > 1) { // 表示是拖拽的 不是自然累加的
-        console.log('大?')
+        console.log('应该显')
         this.progressAutoAdd = false
+        console.log('this.progressAutoAdd', this.progressAutoAdd)
+      }
+    },
+    loading: function(newVal, oldVal) {
+      if (!newVal && newVal != oldVal) {
+        this.triggerPlay()
       }
     }
   },
   methods: {
     init() {
       const that = this
-      console.log('this.currentIndex', this.currentIndex)
       that.videoInstance = document.querySelectorAll('video')[this.currentIndex];
-      let drawTimerInterval = null
-      let currentTimeInterval = null
-      if (that.videoInstance ) {
-         // 播放
-        that.videoInstance.addEventListener('play', function() {
-            window.clearInterval(currentTimeInterval)
-            that.playing = true
-            that.pauseing = false
-            drawTimerInterval = window.setInterval(() => {
-                that.progressAutoAdd = true
-                that.canvasInstance.drawImage(that.videoInstance, 0, 0, 270, 135);
-            }, 20);  // 每0.02秒画一张图片
-
-            currentTimeInterval = window.setInterval(() => {
-              that.progress = that.progress + 1 // 播放条累计
-              that.currentTime = that.durationFormat(that.progress)
-            }, 1000);  // 一秒钟更新一次
-
-        }, false);
-        // 暂停
-        that.videoInstance.addEventListener('pause', function() {
-            that.playing = false
-            that.pauseing = true
-            window.clearInterval(drawTimerInterval);  // 暂停绘画
-            window.clearInterval(currentTimeInterval);  // 暂停计时
-        }, false);
-        // 结束
-        this.videoInstance.addEventListener('ended', ()=> {
-          if (this.currentIndex < this.playList.length) {
-            this.currentIndex++
+      this.drawTimerInterval = null
+      this.currentTimeInterval = null
+      let videoPlay = () => {
+        window.clearInterval(this.currentTimeInterval)
+        this.playing = true
+        this.pauseing = false
+        console.log('this.drawTimerInterval', this.drawTimerInterval)
+        this.drawTimerInterval = window.setInterval(() => {
+            that.canvasInstance.drawImage(that.videoInstance, 0, 0, 400, 200);
+        }, 20);  // 每0.02秒画一张图片
+        this.currentTimeInterval = window.setInterval(() => {
+          // console.log(this.videoInstance.buffered)
+          that.progressAutoAdd = true
+          that.progress = that.progress + 1 // 播放条累计
+          that.currentTime = that.durationFormat(that.progress)
+        }, 1000);  // 一秒钟更新一次
+      }
+      let videoPause = () => {
+          this.playing = false
+          this.pauseing = true
+          clearInterval(this.drawTimerInterval);  // 暂停绘画
+          clearInterval(this.currentTimeInterval);  // 暂停计时
+      }
+      let videoEnded = () => {
+        if (this.currentIndex < this.playList.length-1) {
+          this.currentIndex++
+          this.$nextTick(()=> {
             this.init()
             this.triggerPlay()
-          } else {
-            this.playing = false
-            this.pauseing = true
-          }
-          window.clearInterval(drawTimerInterval);  // 暂停绘画
-          window.clearInterval(currentTimeInterval);  // 暂停计时
-        }, false)
+          })
+        } else {
+          this.playing = false
+          this.pauseing = true
+        }
+        clearInterval(this.drawTimerInterval);  // 暂停绘画
+        clearInterval(this.currentTimeInterval);  // 暂停计时
+      }
+      let videoCanplay = () => {
+         this.loading = false
+         this.playList[this.currentIndex].load = true
+      }
+      if (that.videoInstance ) {
+        // 避免多次绑定,
+        that.videoInstance.removeEventListener('play', videoPlay)
+        that.videoInstance.removeEventListener('pause', videoPause)
+        that.videoInstance.removeEventListener('ended', videoEnded)
+        that.videoInstance.removeEventListener('canplay', videoCanplay)
+
+        // 播放
+        that.videoInstance.addEventListener('play', videoPlay, false);
+        // 暂停
+        that.videoInstance.addEventListener('pause', videoPause, false);
+        // 结束
+        this.videoInstance.addEventListener('ended', videoEnded, false)
         // 可以播放
-        this.videoInstance.addEventListener('canplay', () => {
-           this.loading = false
-           this.playList[this.currentIndex].load = true
+        this.videoInstance.addEventListener('canplay', videoCanplay, false)
+        this.videoInstance.addEventListener('waiting', () => {
+          console.log('粗发waiting')
+           this.loading = true
+           clearInterval(this.currentTimeInterval)
         }, false)
+        // 预先加载下一个视频碎片
         this.videoPreLoad()
       }
     },
@@ -138,10 +160,13 @@ export default {
       // 否则不显示小时 只显示 分和秒
      return ms > 3600 ? moment.duration(ms, "seconds").format('h:m:ss', { trim: false }) : moment.duration(ms, "seconds").format('m:ss', { trim: false })
     },
-    progressChange(val) {
+    progressDrag(val) {
       if(this.progressAutoAdd) return
       this.playing = false
       this.pauseing = true
+      this.videoInstance.pause() // 当拖拽的是 视频应该暂停,不然声音还一直播放
+      clearInterval(this.currentTimeInterval)
+      clearInterval(this.drawTimerInterval)
       if (!val) {
         // 当拖到最前的时候 播放第一个
         this.videoInstance.src = this.playList[0].src;
@@ -151,37 +176,41 @@ export default {
           // 判断当前滑点在哪个视频源上
           let tempLength = 0 // 记录当前滑点之前的视频时间总长度
           if (item.position < val & val < (item.position + item.duration)) {
-            this.videoInstance.src = item.src
-            this.currentIndex = i
+            this.currentIndex = i // 显示当前的video
+            this.init() // 初始化video实例
             this.currentTime = this.durationFormat(val)
             this.videoInstance.currentTime = val - tempLength
             this.loading = !item.load
+            if (item.load) {
+              this.triggerPlay()
+            }
           }
           tempLength = tempLength + item.duration
         })
       }
     },
     triggerPlay() {
-      console.log('播放')
+      clearInterval(this.currentTimeInterval)
+      clearInterval(this.drawTimerInterval)
       if (!this.videoInstance.paused && !this.pauseing) {
-        this.videoInstance.pause();
-        console.log('展厅')
+        console.log('暂停')
+        this.videoInstance.pause()
+        this.pauseing = true
       } else if(this.playList[this.currentIndex].load && !this.playing) {
-        this.videoInstance.play();
-        console.log('kaishi')
+        console.log('开始播放')
+        this.videoInstance.play()
+        this.pauseing = false
       }
-      console.log('this.playList[this.currentIndex].load', this.playList[this.currentIndex].load)
-      console.log('this.videoInstance.paused ', this.videoInstance.paused)
-      console.log('this.playing', this.playing)
-      this.pauseing = !this.pauseing
     },
     videoPreLoad(){
       let preLoadSourceIndex =  this.currentIndex + 1
       if(preLoadSourceIndex < this.playList.length) { // 存在就让他去提前加载
-        let nextVedio = document.querySelectorAll('video')[this.currentIndex]
-        nextVedio.load()
-        nextVedio.addEventListener('canplay', () => {
-          this.playList[this.currentIndex+1].load = true
+        let nextVedio = document.querySelectorAll('video')[preLoadSourceIndex]
+        if (nextVedio && !this.playList[preLoadSourceIndex].load) {
+          nextVedio.load()
+        }
+        nextVedio &&　nextVedio.addEventListener('canplay', () => {
+          this.playList[preLoadSourceIndex].load = true
         }, false)
       }
     }
@@ -210,8 +239,9 @@ a {
 }
 .video-cont{
   position: relative;
-  width: 270px;
-  height: 135px;
+  margin: 0 auto;
+  width: 400px;
+  height: 200px;
   overflow: hidden;
 }
 .video-cont:hover .controls{
@@ -229,16 +259,5 @@ a {
   }
 .time-duration {
   font-size: 0.6rem;
-}
-.sounds-contro {
-  position: absoulte;
-  bottom: 2px;
-  right: 4px;
-  height: 50%;
-  transform: rotate(-90deg);
-  -ms-transform: rotate(-90deg);		/* IE 9 */
-  -webkit-transform: rotate(-90deg);	/* Safari and Chrome */
-  -o-transform: rotate(-90deg);		/* Opera */
-  -moz-transform: rotate(-90deg);		/* Firefox */
 }
 </style>
