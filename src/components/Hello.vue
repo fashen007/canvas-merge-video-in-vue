cc01363edb3300ff6124df2ef0e709dde45c7e99
yonghuang <646028751@qq.com>
2017-02-21 13:54:12 +0800

    新

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -10,9 +10,15 @@
       <div class='controls'>
           <el-row :gutter="5" class='row'>
             <el-col :span="3"><div class="grid-content bg-purple"></div><el-button size="mini" @click.native='clickTrigger'>{{pauseing ? '开始': '暂停'}}</el-button></el-col>
-            <el-col :span="7" class='time-duration'><div class="grid-content bg-purple">{{currentTimeLabel}}/{{terminalTime}}</div></el-col>
+            <el-col :span="7" class='time-duration'><div class="grid-content bg-purple">{{currentTimeLabel}}/{{terminalTimeLabel}}</div></el-col>
             <el-col :span="10"><div class="grid-content bg-purple"><el-slider @change='progressDrag' v-model="progress" :max='allLength'></el-slider></div></el-col>
+            <el-col :span="4">
+              <div class="grid-content bg-purple">
+                 <span class='sound-contr'> <span class='sound-icon' @click='triggerSound'>{{mutedable ? '声音' : '静音'}}</span><input type="range" name="" v-model="sounds" orient="vertical"></span>
+              </div>
+            </el-col>
           </el-row>
+
       </div>
     </div>
 </div>
@@ -21,25 +27,63 @@
 <script>
 import moment from 'moment'
 require('moment-duration-format')
+var drawTimerInterval = null
+var progressInterval = null
 export default {
   name: 'hello',
   data () {
     return {
       autoPlay: false, // 是否自动播放
-      progressAutoAdd: true, // 表示是否是自己播放的还是拖拽后播放的
       currentEnoughToPlay: false, // 表示是否需要显示enoughToPlay状态
       pauseing: true, // 暂停状态
       playing: false, // 播放状态
-      sounds: 10, // 声音控制,暂时还没实现
-      progress: 0,
-      allLength: 134, // 总长度.这个是需要后端返回的
+      sounds: 10, // 声音控制
+      mutedable: false, // 是否静音
+      progress: 0, // 进度条
+      allLength: 0, // 总长度.这个是需要后端返回的
       currentTimeLabel: '0:00', // 默认播放时间 用来显示
-      currentTime: 0,
+      terminalTimeLabel: '', // 终点时间
+      currentTime: 0, // 当前时间
       currentIndex: 0, // 默认当前播放碎片指引
-      terminalTime: '', // 终点
       videoInstance: null, // 当前激活的视频实例
-      canvasInstance: null, // canvas 实例
-      playList: [{ // 碎片资源列表  这个是需要后端返回的
+      canvasInstance: null, // canvas 实
+      playList: []
+    }
+  },
+  mounted () {
+    this.progressSetTimeout = null
+    this.hasPlayTime = 0
+    let c = document.getElementById('myCanvas')
+    this.canvasInstance = c.getContext('2d')
+    this.terminalTimeLabel = this.durationFormat((this.allLength)) // 格式化所有视频长度
+    this.getData()
+  },
+  watch: {
+    // currentEnoughToPlay之后 触发一次播放
+    currentEnoughToPlay: function (newVal, oldVal) {
+      if (newVal && newVal != oldVal) {
+        this.triggerPlay()
+      }
+    },
+    currentTime: function (newVal, oldVal) {
+      const that = this
+      if (newVal != oldVal) {
+        let diff = newVal - oldVal
+        this.hasPlayTime = this.hasPlayTime + (diff > 0 ? (diff > 0.02 ? 0.02 : diff) : 0)
+        this.canvasInstance.drawImage(this.videoInstance, 0, 0, 400, 200)
+        that.progressSetTimeout = window.setTimeout(() => {
+          that.currentTimeLabel = that.durationFormat(Math.floor(this.hasPlayTime))
+        }, 1000)  // 一秒钟更新一次
+      }
+    },
+    sounds: function (newVal, oldVal) {
+      this.videoInstance.volume = newVal / 100
+    }
+  },
+  methods: {
+    getData () {
+      // 后端请求 之后
+      this.playList = [{ // 碎片资源列表  这个是需要后端返回的
         src: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
         duration: 52, // 长度
         position: 0, // 节点
@@ -60,54 +104,21 @@ export default {
         position: 75,
         enoughToPlay: false
       }]
-    }
-  },
-  mounted () {
-    this.progressSetTimeout = null
-    this.hasPlayTime = 0
-    let c = document.getElementById('myCanvas')
-    this.canvasInstance = c.getContext('2d')
-    this.terminalTime = this.durationFormat((this.allLength)) // 格式化所有视频长度
-    this.init()
-  },
-  watch: {
-    // currentEnoughToPlay之后 触发一次播放
-    currentEnoughToPlay: function (newVal, oldVal) {
-      if (newVal && newVal != oldVal) {
-        this.triggerPlay()
-      }
+      this.allLength = 134 // 这里需要用一个for循环去累加 后端返回视屏碎片的长度 我就简单写死了
+      this.$nextTick(() => {
+        this.init()
+      })
     },
-    currentTime: function (newVal, oldVal) {
-      const that = this
-      if (newVal != oldVal) {
-        let diff = newVal - oldVal
-        this.hasPlayTime = this.hasPlayTime + (diff > 0 ? diff : 0)
-        this.canvasInstance.drawImage(this.videoInstance, 0, 0, 400, 200)
-        that.progressSetTimeout = window.setTimeout(() => {
-          that.currentTimeLabel = that.durationFormat(Math.floor(this.hasPlayTime))
-        }, 1000)  // 一秒钟更新一次
-      }
-    }
-  },
-  methods: {
     init () {
       const that = this
-      that.videoInstance = document.querySelectorAll('video')[this.currentIndex]
-      this.drawTimerInterval = null
-      this.progressInterval && clearInterval(this.progressInterval)
-      this.drawTimerInterval && clearInterval(this.drawTimerInterval)
+      this.videoInstance = document.querySelectorAll('video')[this.currentIndex]
+      this.videoInstance.volume = this.sounds / 100
+      this.mutedable = this.videoInstance.muted
       // 视频play监听回调
       let videoPlayHandle = () => {
         this.playing = true
         this.pauseing = false
-        console.log('播放一下呗')
-        this.drawTimerInterval = window.setInterval(() => {
-          this.currentTime = that.videoInstance.currentTime
-        }, 20)  // 每0.02秒画一张图片
-        this.progressInterval = window.setInterval(() => {
-          that.progress = Math.floor(that.hasPlayTime)
-          console.log('我还是执行了')
-        }, 1000)  // 每0.02秒画一张图片
+        this.drawStart()
       }
       // 视频pause监听回调
       let videoPauseHandle = () => {
@@ -126,19 +137,12 @@ export default {
           this.playing = false
           this.pauseing = true
         }
-        // 清除绘制计时器
-        clearInterval(this.drawTimerInterval) // 暂停绘画
+        this.clearIntervaler()
       }
       // 视频canplay监听回调
       let videoCanplayHandle = () => {
         this.currentEnoughToPlay = true
         this.playList[this.currentIndex].enoughToPlay = true
-      }
-      // 视频等待缓冲监听回调
-      let videoWaitingHandle = () => {
-        this.playList[this.currentIndex].enoughToPlay = false
-        this.playing = false
-        this.pauseing = true
       }
       if (that.videoInstance) {
         // 避免多次绑定,
@@ -146,7 +150,6 @@ export default {
         that.videoInstance.removeEventListener('pause', videoPauseHandle)
         that.videoInstance.removeEventListener('ended', videoEndedHandle)
         that.videoInstance.removeEventListener('canplay', videoCanplayHandle)
-        that.videoInstance.removeEventListener('waiting', videoWaitingHandle)
         // 播放
         that.videoInstance.addEventListener('play', videoPlayHandle, false)
         // 暂停
@@ -155,8 +158,6 @@ export default {
         this.videoInstance.addEventListener('ended', videoEndedHandle, false)
         // 可以播放
         this.videoInstance.addEventListener('canplay', videoCanplayHandle, false)
-        // 需要缓冲
-        this.videoInstance.addEventListener('waiting', videoWaitingHandle, false)
         // 预先加载下一个视频碎片
         this.videoPreLoad()
         this.captureFisrt()
@@ -171,6 +172,20 @@ export default {
         }
       })
     },
+    drawStart () {
+      const that = this
+      this.clearIntervaler()
+      if (!drawTimerInterval && this.playing) {
+        drawTimerInterval = window.setInterval(() => {
+          this.currentTime = that.videoInstance.currentTime
+        }, 20)  // 每0.02秒画一张图片
+      }
+      if (!progressInterval && this.playing) {
+        progressInterval = window.setInterval(() => {
+          that.progress = Math.floor(that.hasPlayTime)
+        }, 1000)  // 每1秒画统计一次时间条
+      }
+    },
     /*
       @param ms 秒数
     */
@@ -182,38 +197,41 @@ export default {
     progressDrag (val) {
       if (val == Math.floor(this.hasPlayTime)) return // 表示是自动播放过程
       // 拖拽了
-      clearInterval(this.progressInterval)
-      clearInterval(this.drawTimerInterval)
-      this.playing = false
+      this.clearIntervaler()
       this.pauseing = true
+      this.playing = false
       this.videoInstance.pause() // 当拖拽的是 视频应该暂停,不然声音还一直播放
       // 拖动进度条
       this.playList.map((item, i) => {
         // 判断当前滑点在哪个视频源上
-        let tempLength = 0 // 记录当前滑点之前的视频时间总长度
         if (item.position <= val & val < (item.position + item.duration)) {
+          this.hasPlayTime = val
           if (this.currentIndex != i) { // 显示当前的video
             this.currentIndex = i
             this.init() // 初始化video实例
           }
-          this.hasPlayTime = val
-          this.videoInstance.currentTime = val - tempLength
+          this.videoInstance.currentTime = val - item.position
+          this.triggerPlay()
         }
-        tempLength = tempLength + item.duration
       })
+      this.currentTimeLabel = this.durationFormat(this.hasPlayTime)
     },
     clickTrigger () {
       this.autoPlay = true
       this.triggerPlay()
     },
     triggerPlay () {
-      clearInterval(this.drawTimerInterval)
-      clearInterval(this.progressInterval)
-      if (this.playing) {
-        this.videoInstance.pause()
-      } else {
+      this.clearIntervaler()
+      if (!this.autoPlay) return
+      if (!this.playing && this.pauseing) {
         this.videoInstance.play()
+      } else {
+        this.videoInstance.pause()
       }
+    },
+    triggerSound () {
+      this.mutedable = !this.mutedable
+      this.videoInstance.muted = this.mutedable
     },
     videoPreLoad () {
       let preLoadSourceIndex = this.currentIndex + 1
@@ -226,12 +244,16 @@ export default {
           this.playList[preLoadSourceIndex].enoughToPlay = true
         }, false)
       }
+    },
+    clearIntervaler () {
+      clearInterval(drawTimerInterval) // 暂停绘画
+      clearInterval(progressInterval) // 暂停计时
+      drawTimerInterval = null
+      progressInterval = null
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
@@ -269,5 +291,28 @@ a {
   }
 .time-duration {
   font-size: 0.6rem;
+}
+.sound-contr{
+  position: relative;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 12px;
+  color: #999;
+  border: 1px solid #999;
+  vertical-align: bottom;
+}
+.sound-contr:hover input[type=range][orient=vertical] {
+  display: block;
+}
+input[type=range][orient=vertical]{
+    display: none;
+    position: absolute;
+    bottom: 30px;
+    right: 5px;
+    writing-mode: bt-lr; /* IE */
+    -webkit-appearance: slider-vertical; /* WebKit */
+    width: 8px;
+    height:  80px;
+    padding: 0 5px;
 }
 </style>
